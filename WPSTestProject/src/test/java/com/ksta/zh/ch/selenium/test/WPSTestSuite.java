cba0cdd54a32fcf23e29f77a0f24ff7d27c4a3f8
André Keller <andre.keller@ksta.zh.ch>
2017-05-09 12:57:19 +0200

    testcase for wps created

--- WPSTestProject/src/test/java/com/ksta/zh/ch/selenium/test/WPSTestSuite.java
+++ WPSTestProject/src/test/java/com/ksta/zh/ch/selenium/test/WPSTestSuite.java
@@ -6,6 +6,9 @@ import com.ksta.zh.ch.selenium.DriverFactory;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.BeforeSuite;
+
+import static org.junit.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,7 +18,7 @@ import org.testng.annotations.AfterSuite;
 
 public class WPSTestSuite extends DriverFactory {
 
-	private String appURL = "https://int2.sam.ktzh.ch/";
+	private String baseUrl = "https://int2.sam.ktzh.ch/";
 	private String isiwebuserid = "wpstestuser2";
 	private String isiwebpasswd = "123Wp$_2014";
 	private int timeout = 10;
@@ -25,18 +28,26 @@ public class WPSTestSuite extends DriverFactory {
 	@BeforeSuite
 	private WebDriver login() throws Exception {
 		driver = DriverFactory.getDriver();
-		driver.get(appURL + "/wps/?login");
+		driver.get(baseUrl + "/wps/home.seam");
 		driver.findElement(By.name("isiwebuserid")).clear();
 		driver.findElement(By.name("isiwebuserid")).sendKeys(isiwebuserid);
 		driver.findElement(By.name("isiwebpasswd")).clear();
 		driver.findElement(By.name("isiwebpasswd")).sendKeys(isiwebpasswd);
 		driver.findElement(By.xpath("//div[@id='loginSubmitButton']/input")).click();
+		getWhenVisible(By.xpath("//table[@id='valor_title_tab_id_shifted']/tbody/tr/td[2]/table/tbody/tr/td"));
 		return driver;
 	}
 
 	@Test
 	public void getVal() throws Exception {
-		System.out.println("nur ein Test");
+		driver.get(baseUrl + "/wps/home.seam");
+		assertEquals("Kurslisten-Titel suchen", getWhenVisible(By.xpath("//form[@id='valorSearchForm']/h4")).getText());
+	    driver.findElement(By.xpath("//input[@id='valorSearchForm:searchTitleValidation:searchTitle']")).clear();
+	    driver.findElement(By.xpath("//input[@id='valorSearchForm:searchTitleValidation:searchTitle']")).sendKeys("qiagen");
+	    clickWhenReady(By.xpath("//input[@id='valorSearchForm:search']"));
+	    waitForElementPresent(By.xpath("//td[@id='valor_title_tab_id']/table/tbody/tr/td/h4"));
+	    assertEquals("Qiagen N.V., Venlo NL, Namenaktie", getWhenVisible(By.xpath("//span[@id='valorInfo_titleValue']")).getText());
+
 	}
 
 	private WebElement getWhenVisible(By finder) {
@@ -51,5 +62,8 @@ public class WPSTestSuite extends DriverFactory {
 		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(finder));
 		element.click();
 	}
-
+	private void waitForElementPresent (By finder) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		wait.until(ExpectedConditions.elementToBeClickable(finder));
+	}
 }
